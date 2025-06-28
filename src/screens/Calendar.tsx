01008a971e856ef62ad01c51939c413147fb8414
yuntaengtaeng <k76253218@gmail.com>
2025-06-28 18:25:15 +0900

    feat : Lv2 캘린더 구현 반영

--- src/screens/Calendar.tsx
+++ src/screens/Calendar.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import DefaultScreen from '../components/layout/DefaultScreen';
+import CalendarComponent from '../components/ui/Calendar';
 
 const Calendar = () => {
   return (
     <DefaultScreen>
-      <Text>Calender</Text>
+      <CalendarComponent />
     </DefaultScreen>
   );
 };

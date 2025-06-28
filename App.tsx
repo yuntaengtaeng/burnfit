1a611c659ffe11eff29f459900648d439ba621b3
yuntaengtaeng <k76253218@gmail.com>
2025-06-28 18:30:14 +0900

    design : BottomTabs Header option 제거

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { StyleSheet, View } from 'react-native';
+import { SafeAreaView } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
 import { BottomTabParamList } from './src/navigation';
 import Home from './src/screens/Home';
 import Calendar from './src/screens/Calendar';
@@ -11,13 +12,22 @@ const Tab = createBottomTabNavigator<BottomTabParamList>();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={Home} />
-        <Tab.Screen name="Calendar" component={Calendar} />
-        <Tab.Screen name="Library" component={Library} />
-        <Tab.Screen name="MyPage" component={MyPage} />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <>
+      <StatusBar style="auto" />
+      <SafeAreaView style={{ flex: 1 }}>
+        <NavigationContainer>
+          <Tab.Navigator
+            screenOptions={{
+              headerShown: false,
+            }}
+          >
+            <Tab.Screen name="Home" component={Home} />
+            <Tab.Screen name="Calendar" component={Calendar} />
+            <Tab.Screen name="Library" component={Library} />
+            <Tab.Screen name="MyPage" component={MyPage} />
+          </Tab.Navigator>
+        </NavigationContainer>
+      </SafeAreaView>
+    </>
   );
 }
